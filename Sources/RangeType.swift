7c5b05022083ec4734122f479ec1474f42810a04
Adam Nemecek <adamnemecek@gmail.com>
2016-12-17 15:19:44 -0800

    * intersect implemented

--- Sources/RangeType.swift
+++ Sources/RangeType.swift
@@ -18,14 +18,28 @@ public protocol RangeType {
 }
 
 extension RangeType {
-    public func clamp(_ value: Bound) -> Bound {
-        return min(max(value, lowerBound), upperBound)
+    init(_ bound: Bound) {
+        self.init(uncheckedBounds: (bound, bound))
     }
+
+    init<R: RangeType>(_ other: R) where R.Bound == Bound {
+        self.init(uncheckedBounds: (other.lowerBound, other.upperBound))
+    }
+
 }
 
 extension RangeType {
-    public func overlap(_ other: Self) -> Self {
-        fatalError()
+    public func intersect(_ other: Self) -> Self {
+        guard upperBound > other.lowerBound else {
+            return Self(upperBound)
+        }
+
+        guard other.upperBound > lowerBound else {
+            return Self(lowerBound)
+        }
+        let s = max(lowerBound, other.lowerBound)
+        let e = min(upperBound, other.upperBound)
+        return Self(uncheckedBounds: (s,e))
     }
 }
 
