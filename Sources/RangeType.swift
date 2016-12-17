854b22b57a6fae6cf980c50626b4f1ac05e82592
Adam Nemecek <adamnemecek@gmail.com>
2016-12-17 13:31:35 -0800

    * initial commit

--- Sources/RangeType.swift
+++ Sources/RangeType.swift
@@ -1,4 +1,47 @@
-struct RangeType {
 
-    var text = "Hello, World!"
+public protocol RangeType {
+    associatedtype Bound: Comparable
+
+    init(uncheckedBounds bounds: (lower: Bound, upper: Bound))
+
+    var lowerBound: Bound { get }
+    var upperBound: Bound { get }
+
+    func contains(_ element: Bound) -> Bool
+
+    var isEmpty: Bool { get }
+
+    init(_ other: Range<Bound>)
+
+    func overlaps(_ other: Range<Bound>) -> Bool
+    func overlaps(_ other: ClosedRange<Bound>) -> Bool
+}
+
+extension RangeType {
+    public func clamp(_ value: Bound) -> Bound {
+        return min(max(value, lowerBound), upperBound)
+    }
+}
+
+extension RangeType {
+    public func overlap(_ other: Self) -> Self {
+        fatalError()
+    }
+}
+
+extension Range: RangeType {
+}
+
+extension CountableRange: RangeType {
+}
+
+extension ClosedRange: RangeType {
+    public init(_ other: Range<Bound>) {
+        lowerBound = other.lowerBound
+        upperBound = other.upperBound
+    }
+
+}
+
+extension CountableClosedRange: RangeType {
 }
